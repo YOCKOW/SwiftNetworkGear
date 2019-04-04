c1ebb47a88519b5b9651d8a01a2af5775b0bbe64
YOCKOW <YOCKOW@users.noreply.github.com>
2019-04-04 16:29:55 +0900

    Change Dependency URLs.

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from:"1.4.1"),
-    .package(url:"https://github.com/YOCKOW/SwiftBootstring", from: "1.0.1"),
-    .package(url:"https://github.com/YOCKOW/SwiftExtensions", from: "0.1.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftPublicSuffix", from: "1.1.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftBootstring.git", from: "1.0.1"),
+    .package(url:"https://github.com/YOCKOW/SwiftExtensions.git", from: "0.1.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftPublicSuffix.git", from: "1.1.0"),
     .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "2.0.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftUnicodeSupplement", from: "0.5.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "0.5.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
